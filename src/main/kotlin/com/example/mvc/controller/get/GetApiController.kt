2eec4dc5610aa8b74400aa1620fc5c60e90a7267
yina <yinaofficeg@gmail.com>
2025-05-14 16:34:51 +0900

    kotlin project init

---
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -0,0 +1,15 @@
+package com.example.mvc.controller.get
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class GetApiController {
+
+    @GetMapping("/hello")
+    fun hello(): String{
+        return "hello kotlin"
+    }
+}
